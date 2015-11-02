63790dfd45f68e787ae132dde4ff9d14c9f75e5f
Frédéric Schmitt <fredschmitt57@gmail.com>
2015-11-02 15:28:41 +0100

    modif go.sh

--- WellFollowed/go.sh
+++ WellFollowed/go.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
+
 gulp bowerAssetic
 gulp assetic
 gulp &
+echo "kill $!" > kill.sh
 php app/console gos:websocket:server &
+echo "kill $!" >> kill.sh
 hhvm --mode server -vServer.Type=fastcgi -vServer.Port=9001 &
+echo "kill $!" >> kill.sh
+chmod 755 kill.sh
