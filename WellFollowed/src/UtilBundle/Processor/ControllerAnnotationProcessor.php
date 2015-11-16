8cbdb080067a32e2e6a90053c61f42482f3f6242
sreiss <simon.reiss57@gmail.com>
2015-11-16 21:58:03 +0100

    DateTime regex fixed

--- WellFollowed/src/UtilBundle/Processor/ControllerAnnotationProcessor.php
+++ WellFollowed/src/UtilBundle/Processor/ControllerAnnotationProcessor.php
@@ -39,10 +39,11 @@ class ControllerAnnotationProcessor
                 $filter = new $methodMetadata->filterClass();
                 foreach ($request->query as $queryParam => $value) {
                     if (method_exists($filter, ($methodName = 'set' . ucfirst($queryParam)))) {
-                        if (preg_match('/[0-9]{4}\-[0-1][1-9]-[0-1][1-9]\T[0-9]{2}\:[0-9]{2}\:[0-9]{2}\.[0-9]{3}[Z]?/', $value))
+                        if (preg_match('/[0-9]{4}\-[0-1][0-9]-[0-2][0-9]\T[0-9]{2}\:[0-9]{2}\:[0-9]{2}\.[0-9]{3}[Z]?/', $value)) {
                             $filter->$methodName(new \DateTime($value));
-                        else
+                        } else {
                             $filter->$methodName($value);
+                        }
                     }
                 }
                 $request->attributes->set('filter', $filter);
