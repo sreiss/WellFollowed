c708dd4f45e55ed55e445c25ab0e36396128c107
sreiss <simon.reiss57@gmail.com>
2016-02-09 21:19:18 +0100

    Prod client secret

--- WellFollowed/src/WellFollowed/AppBundle/Resources/src/app/constant/wf-auth-settings.constant.js
+++ WellFollowed/src/WellFollowed/AppBundle/Resources/src/app/constant/wf-auth-settings.constant.js
@@ -1,4 +1,5 @@
 angular.module('wellFollowed').constant('wfAuthSettings', {
    clientId: 'well_followed_web',
-   clientSecret: '9qdnr3vt8n400gco4kcg4gskgwk4okg'
+   clientSecret: '4ivfyiyo6e4gkcg800kc8k8wswwwgc0'
+   //clientSecret: '9qdnr3vt8n400gco4kcg4gskgwk4okg'
 });
