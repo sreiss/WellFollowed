78885cdfc208b2f54b88facf653933e07812459b
Simon Reiss <sreiss@jotunheim>
2016-02-17 22:27:27 +0100

    Start script, run with 'sudo sh start.sh'

--- start.sh
+++ start.sh
@@ -13,10 +13,14 @@ error() {
 
 run_rabbitmq() {
     
-    echo "Starting RabbitMQ..."
-    
-    rabbitmq-server -detached 2>/dev/null
-    
+    if [ ! -f /var/run/rabbitmq/pid ];
+        then
+            echo "Starting RabbitMQ Server..."
+            rabbitmq-server -detached 2>/dev/null
+        else
+            echo "RabbitMQ already running."
+    fi
+
 }
 
 run_gos_websocket() {
@@ -28,6 +32,42 @@ run_gos_websocket() {
             echo $! > /var/run/gos-websocket.pid
         else
             echo "Gos Websocket already running."
+    fi
+    
+}
+
+run_mysql() {
+    
+    if [ ! -f /var/run/mysqld/mysqld.pid ];
+        then
+            echo "Starting MySQL Server..."
+            service mysql start
+        else
+            echo "MySQL already running."
+    fi
+    
+}
+
+run_nginx() {
+    
+    if [ ! -f /var/run/nginx.pid ];
+        then
+            echo "Starting Nginx..."
+            service nginx start
+        else
+            echo "Nginx already running."
+    fi
+    
+}
+
+run_php_fpm() {
+    
+    if [ ! -f /var/run/php5-fpm.pid ];
+        then
+            echo "Starting PHP-FPM..."
+            service php5-fpm start
+        else
+            echo "PHP-FPM already running."
     fi
     
 }
@@ -49,3 +89,9 @@ echo "Starting WellFollowed..."
 run_rabbitmq
 
 run_gos_websocket
+
+run_mysql
+
+run_nginx
+
+run_php_fpm
