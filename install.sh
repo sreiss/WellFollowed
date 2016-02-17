d900ac18e344f0625377bf73cad7d4b84f021739
Simon Reiss <sreiss@jotunheim>
2016-02-17 22:06:55 +0100

    composer installed with www-data

--- install.sh
+++ install.sh
@@ -234,10 +234,10 @@ install_node_npm() {
 }
 
 install_composer() {
-    php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
-    php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === 'fd26ce67e3b237fffd5e5544b45b0d92c41a4afe3e3f778e942e43ce6be197b9cdc7c251dcde6e2a52297ea269370680') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); }"
-    php composer-setup.php
-    php -r "unlink('composer-setup.php');"
+    sudo -u www-data php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+    sudo -u www-data php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === 'fd26ce67e3b237fffd5e5544b45b0d92c41a4afe3e3f778e942e43ce6be197b9cdc7c251dcde6e2a52297ea269370680') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); }"
+    sudo -u www-data php composer-setup.php
+    sudo -u www-data php -r "unlink('composer-setup.php');"
 }
 
 install_wellfollowed() {
